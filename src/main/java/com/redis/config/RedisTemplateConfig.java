8de565b0b9e8926aae025a9a19b1a8f52d7fcfca
chenyu5456 <545641154@qq.com>
2019-02-16 15:14:21 +0800

    Initial commit

---
+++ src/main/java/com/redis/config/RedisTemplateConfig.java
@@ -0,0 +1,31 @@
+package com.redis.config;
+
+import com.redis.serializer.FastJsonRedisSerializer;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author: ChenYu
+ * @Date: 2019/2/16 14:52
+ * @Description:
+ */
+@SpringBootConfiguration
+public class RedisTemplateConfig {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        //使用fastjson序列化
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+        //value值得序列化采用fastJsonRedisSerializer
+        template.setValueSerializer(fastJsonRedisSerializer);
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        //key的序列化采用StringRedisSerializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
